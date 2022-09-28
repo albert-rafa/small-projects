a90b868281704f254dc1b52641da29e5c522b50c
Rafa Albert <albertc.rafaela@gmail.com>
2022-09-28 20:10:41 -0300

    New checker function

--- projects/palindrome/script.js
+++ projects/palindrome/script.js
@@ -2,13 +2,11 @@
 const input = document.querySelector('.input-wrapper input')
 const button = document.querySelector('.input-wrapper button')
 button.addEventListener('click', () => {
-    let text = input.value.toLowerCase().trim()
-
-    let inverse = [];
-    for (let i = 0; i < text.length; i++) {
-        inverse.unshift(text[i])
-    }
+    const text = input.value.toLowerCase().split('').filter(item => {
+        return item.match(/\w/g)
+    })
+    const reverse = text.reverse()
 
     const answer = document.querySelector('.answer')
-    answer.innerText = (text.localeCompare(inverse.toString().replaceAll(',', ''))) ? "IT'S NOT A PALINDROME" : "IT'S A PALINDROME"
+    answer.innerText = (text.toString().localeCompare(reverse.toString())) ? "IT'S NOT A PALINDROME" : "IT'S A PALINDROME"
 })
