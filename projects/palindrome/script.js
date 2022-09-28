9558c495f0a3e02d63e3f3f2b0c6a35c265513d5
Rafa Albert <albertc.rafaela@gmail.com>
2022-09-28 20:36:43 -0300

    Accepts the enter key

--- projects/palindrome/script.js
+++ projects/palindrome/script.js
@@ -1,12 +1,23 @@
 
 const input = document.querySelector('.input-wrapper input')
+input.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        console.log('Opa, enter!')
+        check(input.value)
+    }
+})
+
 const button = document.querySelector('.input-wrapper button')
 button.addEventListener('click', () => {
-    const text = input.value.toLowerCase().split('').filter(item => {
+    console.log('Opa, botão!')
+    check(input.value)
+})
+
+function check(input) {
+    const text = input.toLowerCase(). split('').filter(item => {
         return item.match(/\w/g)
     })
-    const reverse = text.reverse()
 
     const answer = document.querySelector('.answer')
-    answer.innerText = (text.toString().localeCompare(reverse.toString())) ? "IT'S NOT A PALINDROME" : "IT'S A PALINDROME"
-})
+    answer.innerText = text.toString().localeCompare(text.reverse().toString()) ? "IT'S NOT A PALINDROME" : "IT'S A PALINDROME"
+}
